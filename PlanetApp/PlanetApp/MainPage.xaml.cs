e5c14f9a5ead5cff914e410bd9183670459399ce
Luis <40400309+krasorx@users.noreply.github.com>
2019-10-23 13:40:32 -0300

    Add files via upload

---
+++ PlanetApp/PlanetApp/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PlanetApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void NewGame_OnClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NewGamePage());
+        }
+
+        private async void loadGame_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoadGamePage());
+        }
+    }
+}
